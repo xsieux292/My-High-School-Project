748b816361d0940723da3a5b16b9b35c2ed279a4
xsieux292 <xsiseux292@gmail.com>
2025-04-17 21:56:41 +0700

    Initial Commit

---
+++ My-Project-High-School/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  My-Project-High-School
+//
+//  Created by Aphichit's Macbook on 17/4/2568 BE.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
